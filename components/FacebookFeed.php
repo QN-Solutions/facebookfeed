2d313ff758aab437f87494732a6ee64ccc783a9d
Christian Kuhn <c.kuhn@qn-solutions.de>
2018-03-23 16:11:01 +0100

    provide at least one available text as short teaser text for a post

--- components/FacebookFeed.php
+++ components/FacebookFeed.php
@@ -73,17 +73,24 @@ class FacebookFeed extends ComponentBase{
 
 				$num = 1;
 
-				foreach ($posts as &$post){
+				foreach ($posts as &$post) {
 
 					// load only a limited number of posts (maxItems) 
 					if($this->property('maxItems') < $num++) break;
+
+					// skip if we have no link for this post
+					if(!isset($post->link)) continue;
 
 					if (isset($post->full_picture))
 							$post->image_link = $post->full_picture;
 					if (isset($post->message))
 						$post->short = substr ($post->message, 0, $this->property('maxDescription')).'...';
+					else if(isset($post->description))
+						$post->short = substr($post->description, 0, $this->property('maxDescription')).'...';
+					else if(isset($post->story))
+						$post->short = substr($post->story, 0, $this->property('maxDescription')).'...';
 					else
-						$post->short = '';
+						$post->short = 'Dieser Post enthält keinen Text.';
 
 					array_push($result, $post);
 				}
